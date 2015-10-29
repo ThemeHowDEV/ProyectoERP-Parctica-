088c0d7b63a4eb65d2f3f775354418496d8b0047
fercilloso <rarfernando@gmail.com>
2015-10-29 11:07:10 -0600

    Estilos
    Done!

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <html>
-<head><title>:::::Ingreso:::::</title></head>
-<body>
-<form action='validar.php' method='POST'>
-
-<input type='text' placeholder="user"  name='user'/><br>
-<input type='password'  name ='pass'/>
-<input type='submit'  >
-
-<br><a title="Recuperar contraseña" href="Email/RecoveryPass/index.php">Olvido su contraseña?</a>
-</form>
-</body>
+	<head>
+		<title>:::::Ingreso:::::</title>
+		
+		<!--IMPORTAMOS FUENTES DE GOOGLE-->
+		<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+		
+		<!--IMPORTAMOS CSS-->
+		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="css/text.css" media="screen" />
+	</head>
+		<body>
+			<form action='validar.php' method='POST' name="login">
+			
+			<div id="cuadros" align="center" class="fuentes">
+			<br/>			
+			<div id="fuentes">
+				Usuario&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
+				<input type='text' name='user' size="40" /><br/><br/>
+				Contrase&ntilde;a</td>&nbsp;:&nbsp;
+				<input type='password' name ='pass' size="40"/><br/><br/>				
+				<a href="#" onclick="document.forms['login'].submit();">Iniciar Session</a>&nbsp;&nbsp;-&nbsp;&nbsp;
+				<a title="Recuperar contraseña" href="Email/RecoveryPass/index.php">Olvide mi contrase&ntilde;a</a>	
+			</div>
+			<br/><br/>
+			</div>			
+		</form>
+		</body>
 </html>
